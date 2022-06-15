4163d288c84e02b1799f8f92726ae8de58081f36
Martin <mkmartinoes@gmail.com>
2022-06-15 12:25:49 +0300

    completed

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -35,7 +35,7 @@ describe "Artist" do
   end
 
   it "can roll back to have no favorite_food attribute for Artist" do    
-    expect(Artist.column_names).not_to include("favorite_food")
+    expect(Artist.column_names).not_to include("favorite_foood")
   end
 
   it "can migrate to have a favorite_flower attribute for Artist" do    
